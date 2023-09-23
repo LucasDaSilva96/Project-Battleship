47e97d888961b38d13c5802bfb8d3ab85eba5428
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-23 21:32:01 +0200

    Removed console.log from the methods

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -15,27 +15,28 @@ export class GAME_BOARD {
     if (orientation === "horizontal") {
       if (col + ship.length > this.columns) {
         // TODO******
-        console.log("Ship cannot fit horizontally at the specified position.");
+        // console.log("Ship cannot fit horizontally at the specified position.");
         return false;
       }
       for (let i = col; i < col + ship.length; i++) {
         if (this.board[row][i] !== "empty") {
-          console.log("There is already a ship in the specified position.");
+          // console.log("There is already a ship in the specified position.");
           return false;
         }
       }
       // Place the ship horizontally
       for (let i = col; i < col + ship.length; i++) {
         this.board[row][i] = ship;
+        return true;
       }
     } else if (orientation === "vertical") {
       if (row + ship.length > this.rows) {
-        console.log("Ship cannot fit vertically at the specified position.");
+        // console.log("Ship cannot fit vertically at the specified position.");
         return false;
       }
       for (let i = row; i < row + ship.length; i++) {
         if (this.board[i][col] !== "empty") {
-          console.log("There is already a ship in the specified position.");
+          // console.log("There is already a ship in the specified position.");
           return false;
         }
       }
@@ -43,21 +44,22 @@ export class GAME_BOARD {
       for (let i = row; i < row + ship.length; i++) {
         this.board[i][col] = ship;
       }
+      return true;
     }
-    return true;
   }
 
   // Function to attack a position on the board
-  receiveAttack(row, col) {
+  receiveAttack(row, col, player) {
     if (this.board[row][col] === "empty") {
       this.board[row][col] = "miss";
-      console.log("Miss");
       return "miss";
     } else if (this.board[row][col] === "hit") {
       console.log("You already hit this position");
     } else {
+      player.hit(this.board[row][col].type);
+      player.isSunk(this.board[row][col].type);
+      player.isGameOver();
       this.board[row][col] = "hit";
-      console.log("Hit");
       return "hit";
     }
     if (this.board[row][col].numberOfHits === this.board[row][col].length) {
