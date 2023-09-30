5245da33b9b5cf8e2ffe9e562f7e2ea2fc5b779c
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-09-30 20:02:21 +0200

    Created the createGameBoard function, this function takes the html game-board and creates an 2D array of it

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,12 +1,8 @@
 export class GAME_BOARD {
-  constructor(rows, columns) {
+  constructor(rows, columns, board) {
     this.rows = rows;
     this.columns = columns;
-    this.board = [];
-    // Initialize the board with empty cells
-    for (let i = 0; i < rows; i++) {
-      this.board.push(Array(columns).fill("empty"));
-    }
+    this.board = board;
   }
 
   // Function to place a ship on the board
@@ -19,13 +15,14 @@ export class GAME_BOARD {
         return false;
       }
       for (let i = col; i < col + ship.length; i++) {
-        if (this.board[row][i] !== "empty") {
+        if (!this.board[row][i].className === "cell empty") {
           // console.log("There is already a ship in the specified position.");
           return false;
         }
       }
       // Place the ship horizontally
       for (let i = col; i < col + ship.length; i++) {
+        this.board[row][i].className = "cell";
         this.board[row][i] = ship;
         return true;
       }
@@ -35,13 +32,14 @@ export class GAME_BOARD {
         return false;
       }
       for (let i = row; i < row + ship.length; i++) {
-        if (this.board[i][col] !== "empty") {
+        if (!this.board[row][i].className === "cell empty") {
           // console.log("There is already a ship in the specified position.");
           return false;
         }
       }
       // Place the ship vertically
       for (let i = row; i < row + ship.length; i++) {
+        this.board[row][i].className = "cell";
         this.board[i][col] = ship;
       }
       return true;
@@ -50,16 +48,16 @@ export class GAME_BOARD {
 
   // Function to attack a position on the board
   receiveAttack(row, col, player) {
-    if (this.board[row][col] === "empty") {
-      this.board[row][col] = "miss";
+    if (this.board[row][col].className === "cell") {
+      this.board[row][col].className = "cell miss";
       return "miss";
-    } else if (this.board[row][col] === "hit") {
+    } else if (this.board[row][col].className === "cell hit") {
       console.log("You already hit this position");
     } else {
       player.hit(this.board[row][col].type);
       player.isSunk(this.board[row][col].type);
       player.isGameOver();
-      this.board[row][col] = "hit";
+      this.board[row][col].className = "cell hit";
       return "hit";
     }
     if (this.board[row][col].numberOfHits === this.board[row][col].length) {
@@ -67,4 +65,33 @@ export class GAME_BOARD {
       return "You sank a ship!";
     }
   }
+}
+
+// Create a 2D array to represent the game board
+export function createGameBoard() {
+  const gameBoardArray = [];
+  // The game board container
+  const gameBoardContainer = document.querySelector(".place-ships-game-board");
+  // All rows within the game board
+  const rows = gameBoardContainer.querySelectorAll(".col");
+  // Iterate through the rows
+  rows.forEach((colElement) => {
+    // Create an array to represent a row
+    const rowArray = [];
+
+    // Select all cells within the row
+    const cells = colElement.querySelectorAll(".cell");
+
+    // Iterate through the cells in the row
+    cells.forEach((cellElement) => {
+      cellElement.className = "cell empty";
+      // Add each cell to the row array
+      rowArray.push(cellElement);
+    });
+
+    // Add the row array to the game board array
+    gameBoardArray.push(rowArray);
+  });
+
+  return gameBoardArray;
 }
